2d4f3e0865ba4fae52d85cf2f0b5b7a443ef13ce
usa4cc <63764542+usa4cc@users.noreply.github.com>
2021-03-24 12:42:06 -0400

    Updated filter
    Simplified code.

--- Fake Word Dictionary/Fake Word Dictionary/Filter.cs
+++ Fake Word Dictionary/Fake Word Dictionary/Filter.cs
@@ -1,108 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Text;
-using System.IO;
-using System.Diagnostics;
 
 namespace Fictionary
 {
+    /// <summary>
+    /// Contains functions to remove vulgar words from database
+    /// </summary>
     public static class Filter
     {
-        private static string[] blacklist =
+        /// <summary>
+        /// The list of words to remove
+        /// </summary>
+        private static readonly string[] blacklist =
         {
             "asshole","bitch","bullshit", "cock","damn","effing",
             "fuck","shit","nigga","nigger", "prick","slut"
         };
 
         /// <summary>
-        /// Checks to see if a message contains a bad word. Filters out leet (replacing letters with numbers), spaces, and uppercase letters.
+        /// Checks to see if a string contains a bad word.
+        /// Also filters out leet, spaces, and uppercase letters.
         /// </summary>
-        public static bool IsClean(string message)
+        /// <param name="str">The string to check for bad words</param>
+        public static bool IsClean(string str)
         {
-            message = message
-                // leet replacements
-                .Replace("4", "a")
-                .Replace("8", "b")
-                .Replace("3", "e")
-                .Replace("9", "g")
-                .Replace("1", "l")
-                .Replace("0", "o")
-                .Replace("2", "r")
-                .Replace("5", "s")
-                .Replace("7", "t")
-                // remove spaces in case people just insert a 
-                // space in the middle of a bad word.
-                .Replace(" ", "")
-                .ToLower();
+            StringBuilder builder = new StringBuilder(str.ToLower());
+
+            // Filter out leet
+            builder.Replace("4", "a");
+            builder.Replace("8", "b");
+            builder.Replace("3", "e");
+            builder.Replace("9", "g");
+            builder.Replace("1", "l");
+            builder.Replace("0", "o");
+            builder.Replace("2", "r");
+            builder.Replace("5", "s");
+            builder.Replace("7", "t");
+            builder.Replace(" ", "");
+
+            // Set string to modified version
+            str = builder.ToString();
 
             // Loop through the blacklist and see if the message has any bad words.
             foreach (string badWord in blacklist)
             {
-                if (message.Contains(badWord))
+                if (str.Contains(badWord))
                 {
                     // If it does, the message is not clean.
                     return false;
                 }
             }
 
-            // If it doesn't, the message is clean.
+            // If we've reached this point, the message is clean
             return true;
         }
 
         /// <summary>
-        /// Replaces instances of bad words with a "?"
-        /// NOTE: does not catch bad words with a space in the middle!
+        /// Replaces instances of bad words with a "?".
         /// </summary>
-        public static string RemoveBadWords(string message)
+        /// <param name="str">The string to remove bad words from</param>
+        public static string RemoveBadWords(string str)
         {
-            // a copy of the message with wierd characters replaced
-            // with normal ones for filtering purposes
-            string normalizedMessage = message
-                // leet replacements
-                .Replace("4", "a")
-                .Replace("8", "b")
-                .Replace("3", "e")
-                .Replace("9", "g")
-                .Replace("1", "l")
-                .Replace("0", "o")
-                .Replace("2", "r")
-                .Replace("5", "s")
-                .Replace("7", "t")
-                .ToLower();
+            StringBuilder builder = new StringBuilder(str.ToLower());
 
-            // a StringBuilder with the normal message so the 
-            // wierd character replacements don't show up in the 
-            // final output
-            var cleanMessageSB = new StringBuilder();
-            cleanMessageSB.Append(message);
+            // Filter out leet
+            builder.Replace("4", "a");
+            builder.Replace("8", "b");
+            builder.Replace("3", "e");
+            builder.Replace("9", "g");
+            builder.Replace("1", "l");
+            builder.Replace("0", "o");
+            builder.Replace("2", "r");
+            builder.Replace("5", "s");
+            builder.Replace("7", "t");
+            builder.Replace(" ", "");
 
-            foreach (string badWord in blacklist)
+            // Now remove bad words
+            foreach (string item in blacklist)
             {
-                // if there is an occurence of the bad word, replace it with ???s
-                while (normalizedMessage.IndexOf(badWord) != -1)
-                {
-                    // a string of ???s the same length as badWord
-                    string replacement = new string('?', badWord.Length);
-
-                    int i = normalizedMessage.IndexOf(badWord);
-
-                    normalizedMessage = normalizedMessage.Remove(i, badWord.Length);
-                    normalizedMessage = normalizedMessage.Insert(i, replacement);
-
-                    cleanMessageSB.Remove(i, badWord.Length);
-                    cleanMessageSB.Insert(i, replacement);
-                }
+                builder.Replace(item, "?");
             }
 
-            string cleanMessage = cleanMessageSB.ToString();
-            // replace strings of ??? to just one ?
-            while (cleanMessage.IndexOf("??") != -1)
-            {
-                cleanMessage = cleanMessage.Replace("??", "?");
-            }
-
-            // return the clean message
-            return cleanMessage;
+            // Return the clean message
+            return builder.ToString();
         }
     }
 }

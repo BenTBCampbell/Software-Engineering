6566378ffb4193ce5eefc64f09321ee9d2b7caff
usa4cc <usa4cc@gmail.com>
2021-04-05 23:23:20 -0400

    Updated views

--- Fictionary/Fictionary/Views/HomeView.xaml.cs
+++ Fictionary/Fictionary/Views/HomeView.xaml.cs
@@ -18,45 +18,5 @@ namespace Fictionary.Views
             BindingContext = viewModel;
             InitializeComponent();
         }
-
-        private async void Account_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<AccountView>());
-        }
-
-        private async void CreateAccount_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<CreateAccountView>());
-        }
-
-        private async void CreateWord_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<CreateWordView>());
-        }
-
-        private async void Credits_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<CreditsView>());
-        }
-
-        private async void Details_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<DetailsView>());
-        }
-
-        private async void Search_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<SearchView>());
-        }
-
-        private async void Settings_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<SettingsView>());
-        }
-
-        private async void SignIn_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(Resolver.Resolve<SignInView>());
-        }
     }
 }

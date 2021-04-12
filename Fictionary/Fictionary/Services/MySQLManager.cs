233a00ec7ece8a8e116db72facfe07f650059d38
Ben Campbell <ben@campbellfam.com>
2021-04-12 17:53:44 -0400

    Add MySQLManager to run queries simply
    Fictionary.Services.MySQLManager
    
    ExecuteQuery(string command): returns a datareader with the response.
    ExecuteNonQuery(string command)

---
+++ Fictionary/Fictionary/Services/MySQLManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using MySqlConnector;
+using PCLAppConfig;
+
+namespace Fictionary.Services
+{
+    /// <summary>
+    /// Contains functions to access the database
+    /// </summary>
+    public static class MySqlManager
+    {
+        private static MySqlConnection Connection;
+
+        private static string dbConnection =
+            $"server={ConfigurationManager.AppSettings["database-ip"]};" +
+            $"uid={ConfigurationManager.AppSettings["database-username"]};" +
+            $"pwd={ConfigurationManager.AppSettings["database-password"]};" +
+            $"database={ConfigurationManager.AppSettings["database-name"]}";
+
+        static MySQLManager()
+        {
+            Connection = new MySqlConnection(dbConnection);
+            Connection.Open();
+        }
+
+        public static MySqlDataReader ExecuteQuery(string command)
+        {
+            // Create the command object, to execute SQL commands
+            var sqlCommand = new MySqlCommand() { Connection = Connection, CommandText = command };
+
+            // Gets the words that are currently in the database
+            return sqlCommand.ExecuteReader();
+        }
+
+        public static int ExecuteNonQuery(string command)
+        {
+            var sqlCommand = new MySqlCommand() { Connection = Connection, CommandText = command };
+            return sqlCommand.ExecuteNonQuery();
+        }
+    }
+}

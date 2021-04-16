09849ac931adacdf64cdad8e8c5af373afa51b11
usa4cc <usa4cc@gmail.com>
2021-04-16 14:25:07 -0400

    Created sample converter--not functional

---
+++ Fictionary/Fictionary/Converters/NameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Fictionary.Converters
+{
+    public class NameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)parameter == "Last")
+            {
+               
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)parameter =="Last")
+            {
+                //return 
+            }
+            return value;
+        }
+    }
+}

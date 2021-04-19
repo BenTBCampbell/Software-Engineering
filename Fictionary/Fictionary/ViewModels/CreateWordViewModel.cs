7cf70a706246ba3300f47ebf4130cddcd67a1260
usa4cc <usa4cc@gmail.com>
2021-04-19 19:12:59 -0400

    Set up binding & logic for adding words

--- Fictionary/Fictionary/ViewModels/CreateWordViewModel.cs
+++ Fictionary/Fictionary/ViewModels/CreateWordViewModel.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using Fictionary.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Fictionary.Services;
 
 namespace Fictionary.ViewModels
 {
     public class CreateWordViewModel : ViewModel
     {
+        /// <summary>
+        /// The definition object for adding words
+        /// </summary>
+        public Definition Definition { get; set; }
+
         public ICommand AddWordCommand => new Command(() =>
         {
-            // TODO: Code needed to add word to database
+            // Add the word to the database
+            WordService.AddDefinition(Definition.Word.WordText, Definition.DefinitionText);
             Navigation.PopModalAsync();
         });
 
